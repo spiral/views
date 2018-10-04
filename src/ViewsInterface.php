802d81c8c77ef6220074365e8ccff76b30fbb6be
Wolfy-J <wolfy.jd@gmail.com>
2018-10-04 14:45:51 +0300

    native engine

--- src/ViewsInterface.php
+++ src/ViewsInterface.php
@@ -8,7 +8,17 @@
 
 namespace Spiral\Views;
 
+use Spiral\Views\Exception\ViewException;
+
 interface ViewsInterface
 {
-
+    /**
+     * Get instance of view class associated with view path (path can include namespace).
+     *
+     * @param string $path
+     * @return ViewInterface
+     *
+     * @throws ViewException
+     */
+    public function get(string $path): ViewInterface;
 }
