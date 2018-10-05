8d174a0e15ceff91cb49e18a6142eaf877abd558
Wolfy-J <wolfy.jd@gmail.com>
2018-10-05 15:43:21 +0300

    more tests

---
+++ tests/Views/ConfigTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+namespace Spiral\Views\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+use Spiral\Views\Config\ViewsConfig;
+
+class ConfigTest extends TestCase
+{
+    public function testCache()
+    {
+        $config = new ViewsConfig([
+            'config' => [
+                'enable'    => true,
+                'directory' => '/tmp'
+            ]
+        ]);
+
+        $this->assertTrue($config->cacheEnabled());
+        $this->assertSame('/tmp', $config->cacheDirectory());
+    }
+}
